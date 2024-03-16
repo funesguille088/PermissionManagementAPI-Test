8a92141ec28b176cdf63ddd05868e8084fe0382b
funesguille088 <guillermo.funes@outlook.com>
2024-03-16 16:42:47 -0600

    PermissionDTO created

---
+++ src/Services/Permissions.Application/Dtos/PermissionDTO.cs
@@ -0,0 +1,13 @@
+﻿
+using Permissions.Domain.Enums;
+
+namespace Permissions.Application.Dtos;
+
+public record OrderDto(
+    Guid Id,
+    Guid EmployeeId,
+    string ApplicationName,
+    PermissionType PermissionType,
+    bool PermissionGranted,
+    Guid PermissionGrantedEmployeeId);
+
